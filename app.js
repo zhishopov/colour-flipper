7dfcfa65649f54e9321ad35ad5623e4e9a75809e
Zhivko Shopov <zhishopov@gmail.com>
2024-12-10 19:31:39 +0000

    Main functionality implemented

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+const redButton = document.getElementById("red");
+const blueButton = document.getElementById("blue");
+const greenButton = document.getElementById("green");
+
+redButton.addEventListener("click", changeToRed);
+blueButton.addEventListener("click", changeToBlue);
+greenButton.addEventListener("click", changeToGreen);
+
+function changeToRed() {
+  const body = document.getElementsByTagName("body")[0];
+  body.style.backgroundColor = "red";
+}
+
+function changeToBlue() {
+  const body = document.getElementsByTagName("body")[0];
+  body.style.backgroundColor = "blue";
+}
+
+function changeToGreen() {
+  const body = document.getElementsByTagName("body")[0];
+  body.style.backgroundColor = "green";
+}
